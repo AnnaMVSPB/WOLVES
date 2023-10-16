29396a19f1f6d45b1878faf5669aa87252343735
AnnaMVSPB <makarobaanya@yandex.ru>
2023-10-16 08:53:36 +0300

    dd

--- client/src/features/transport/reducer/action.ts
+++
@@ -1,7 +0,0 @@
-import type { Transport, TransportId } from "../Type/transport";
-
-export type Action =
- | { type: 'transports/init'; payload: Transport[] }
- | { type: 'transports/deleTransport'; payload: TransportId }
- | { type: 'transports/updateTransport'; payload: Transport }
- | { type: 'transports/addTransport'; payload: Transport }
